65dc275e7aa59e0d2271886ffc0a542e1fbf25ae
unknown <asaulenkojan@gmail.com>
2020-01-30 12:27:56 +0200

    created view of feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Feed = ({ articles }) =>
-{
+const Feed = ({ articles }) => {
   return (
     <div>
-      { articles.map((article, index) =>
-      {
+      {articles.map((article, index) => {
         return (
-          <div key={ index }>
-            <div>{ article.title }</div>
+          <div className="article-preview " key={index}>
+            <div className="article-meta">
+              <Link to={`/profiles/${ article.author.username }`}>
+                <img src={article.author.image} alt="user_image" />
+              </Link>
+              <div className="info">
+                <Link to={`/profile/${ article.author.username }`} className="author">
+                  {article.author.username}
+                </Link>
+                <span className="date">{article.createdAt}</span>
+              </div>
+            </div>
+            <Link to={`/articles/${ article.slug }`} className="preview-link">
+              <h1>{article.title}</h1>
+              <p>{article.description}</p>
+              <span>Read more ...</span>
+              <ul className="tag-list">
+                {article.tagList.map(tag => {
+                  return <li className="tag-default tag-pill tag-outline" key={tag}>{tag}</li>
+                })}
+              </ul>
+            </Link>
           </div>
-        );
-      }) }
-    </div>
-  );
-};
+        )
+      })
+      }
+    </div>)
+}
 
 export default Feed;
--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -24,7 +24,14 @@ const GlobalFeed = () => {
             {error && <div>Some error</div>}
             {!isloading && response && <Feed articles={response.articles} />}
           </div>
-          <div className="col-md-3">Popular tags</div>
+          <div className="col-md-3">Popular tags
+            <div className="row">
+              <div className="col-md-4">Latest</div>
+              <div className="col-md-3"> Most liked</div>
+              <div className="col-md-3">Ukrainian</div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
